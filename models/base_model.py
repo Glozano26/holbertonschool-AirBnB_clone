7c7282193b4bda7a8df8c3952e61b29d41ec0aad
Glozano26 <135636906+Glozano26@users.noreply.github.com>
2023-11-02 17:43:44 -0500

    "Create BaseModel from dictionary"

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
 """a class model"""
 import uuid
-import datetime
+from datetime import datetime
 
 
 
 class BaseModel():
     """a class BaseModel that defines all common attributes/methods for other classes"""
-    
-    
     def __init__(self, *args, **kwargs):
         """initit BaseModel"""
-        
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
-        
-    if kwargs != {}:
-        for key, value in kwargs.items():
-            if key == "id":
-                self.id = value
-            elif key == "created_at":
-                self.creeted_at = value
-            elif key == "updated_at":
-                self.updated_at = value
-    else:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs != {}:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "created_at":
+                    self.created_at = value
+                elif key == "updated_at":
+                    self.updated_at = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """assign with the current datetime and will be updated"""
