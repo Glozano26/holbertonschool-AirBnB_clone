1aa2ed1350c7088ca14a95acc4bc448c0450a1e1
Glozano26 <135636906+Glozano26@users.noreply.github.com>
2023-11-02 20:46:21 -0500

    "v6"

--- models/base_model.py
+++ models/base_model.py
@@ -21,10 +21,12 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.save()
 
     def save(self):
         """assign with the current datetime and will be updated"""
         self.update_at = datetime.datetime.now()
+        models.storage.save()
         
     def __str__(self):
         """Update a dict"""
