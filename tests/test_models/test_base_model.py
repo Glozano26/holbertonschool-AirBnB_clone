67f770273f5e2061a8ba3b8447a50369c86815ab
Glozano26 <lozsuperbia2014@gmail.com>
2023-11-05 23:48:06 -0500

    base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,107 +1,70 @@
 #!/usr/bin/python3
-"""Unittest for basemodel file: class and methods"""
-
-import pep8
-import unittest
+"""Unittest module for the class BaseModel
+"""
 import os
-from models import base_model
-from models.base_model import BaseModel
-from models.user import User
+import unittest
 from datetime import datetime
+from models.base_model import BaseModel
+import models
 
 
-class Test_Base_Model_outputs(unittest.TestCase):
-    """Test_Base_outputs test for Base class"""
-
-    def test_unique_id(self):
+class TestBaseModel(unittest.TestCase):
+    """methods of the test for class BaseModel
+    """
+    def test_documentation(self):
+        """this checks all the documentation
+        of all the methods of the BaseModel class
         """
-        test_unique_id method that test if id is unique
+        self.assertIsNotNone(models.base_model.__doc__)
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_from_dict(self):
+        """test from dict"""
+        b = BaseModel()
+        b.name = "Holberton"
+        b.my_number = 89
+        d1 = b.to_dict()
+        b2 = BaseModel(**d1)
+        self.assertIsNot(b, b2)
+        self.assertIsInstance(b2, BaseModel)
+        self.assertEqual(b2.id, b.id)
+        self.assertEqual(b2.created_at, b.created_at)
+        self.assertEqual(b2.updated_at, b.updated_at)
+        self.assertEqual(b2.name, b.name)
+        self.assertEqual(b2.my_number, b.my_number)
+
+    def test_uniqueId(self):
+        """this check if the instance
+        that are created has a unique id
         """
         instance1 = BaseModel()
         instance2 = BaseModel()
         self.assertNotEqual(instance1, instance2)
+        self.assertNotEqual(instance1.id, instance2.id)
 
-    def test_id_type(self):
-        """
-        test_id_type method that test if type of id is correct
-        """
-        instance1 = BaseModel()
-        self.assertEqual('<class \'str\'>', str(type(instance1.id)))
-
-    def test_exec_file(self):
-        """
-        test_exec_file method to test if file has read, write and exec
-        permissions
+    def test_exec_permissions(self):
+        """Method that review if the file is executable
         """
         read = os.access('models/base_model.py', os.R_OK)
-        self.assertEqual(True, read)
+        self.assertTrue(read)
         write = os.access('models/base_model.py', os.W_OK)
-        self.assertEqual(True, write)
-        exec = os.access('models/base_model.py', os.X_OK)
-        self.assertEqual(True, exec)
+        self.assertTrue(write)
+        exect = os.access('models/base_model.py', os.X_OK)
+        self.assertTrue(exect)
 
-    def test_save(self):
-        """
-        test_save method to test if each time that the instance is
-        saved the update_at attribute is updated
-        """
-        instance1 = BaseModel()
-        attr_updated_before_save = instance1.updated_at
-        instance1.save()
-        attr_updated_after_save = instance1.updated_at
-        self.assertNotEqual(attr_updated_before_save, attr_updated_after_save)
-
-    def test_to_dict(self):
-        """
-        test_to_dict method that test if a dictionary is returned
-        and if updated_at and created_at attributes are in the correct
-        format
+    def test_typeData(self):
+        """this method check the type of
+        the atributes when created a instance
         """
         instance1 = BaseModel()
-        instance1_User = User()
-        # test type of return
-        self.assertEqual('<class \'dict\'>', str(type(instance1.to_dict())))
+        self.assertIsInstance(instance1.created_at, datetime)
+        self.assertIsInstance(instance1.updated_at, datetime)
+        self.assertIsInstance(instance1.id, str)
+        self.assertIsInstance(instance1, BaseModel)
 
-        updated_expected_format = instance1.updated_at.isoformat()
-        created_expected_format = instance1.created_at.isoformat()
-        class_attr_value_expected = type(instance1_User).__name__
-        updated_actual_format = instance1.to_dict()["updated_at"]
-        created_actual_format = instance1.to_dict()["created_at"]
-        class_attr_value_get = instance1_User.to_dict()['__class__']
-        # test format inside the dictionary
-        self.assertEqual(updated_expected_format, updated_actual_format)
-        self.assertEqual(created_expected_format, created_actual_format)
-        self.assertEqual(class_attr_value_expected, class_attr_value_get)
-
-
-class TestBaseModelpep8(unittest.TestCase):
-    """Validate pep8"""
-
-    def test_pep8(self):
-        """test for base file and test_base file pep8"""
-        style = pep8.StyleGuide(quiet=True)
-        base_mod = "models/base_model.py"
-        test_base_mod = "tests/test_models/test_base_model.py"
-        result = style.check_files([base_mod, test_base_mod])
-        self.assertEqual(result.total_errors, 0)
-
-
-class TestDocsBaseModel(unittest.TestCase):
-    """test docstrings for base and test_base files"""
-
-    def test_module(self):
-        """check module docstrings"""
-        self.assertTrue(len(base_model.__doc__) > 0)
-
-    def test_class(self):
-        """check class docstrings"""
-        self.assertTrue(len(BaseModel.__doc__) > 0)
-
-    def test_method(self):
-        """check method docstrings"""
-        for func in dir(BaseModel):
-            self.assertTrue(len(func.__doc__) > 0)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
